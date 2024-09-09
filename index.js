0a4f40eba27213885ac3d14dc0152621fe44e71c
amber-emmes <ablaylock@emmes.com>
2024-09-09 09:14:49 -0400

    Removed erroneously included lines for local API usage + console log

--- index.js
+++ index.js
@@ -71,8 +71,6 @@ window.onload = async () => {
     else {
         !firebase.apps.length ? firebase.initializeApp(devFirebaseConfig) : firebase.app();
         !isLocalDev && window.DD_RUM && window.DD_RUM.init({ ...datadogConfig, env: 'dev' });
-        // TODO: Remove this line 
-        if (location.host.startsWith('localhost')) firebase.functions().useFunctionsEmulator('http://localhost:5001'); 
     }
 
     !isLocalDev && window.DD_RUM && window.DD_RUM.startSessionReplayRecording();
