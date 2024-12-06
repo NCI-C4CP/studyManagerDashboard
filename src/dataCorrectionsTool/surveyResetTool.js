70117f63a0d2186050bcc4cba3d4607d946d34b2
Gene Barra <barragj@nih.gov>
2024-12-06 13:42:32 -0500

    add text feedback for ssn survey reset

--- src/dataCorrectionsTool/surveyResetTool.js
+++ src/dataCorrectionsTool/surveyResetTool.js
@@ -52,14 +52,12 @@ const renderDataCorrectionsSelectionContent = (participant) => {
                     </div>
                 </div>
 
-
                 <div class="row">
                     <div class="col my-2">
                         <h2 class="norcToolTypeHeader"> Survey Status Reset </h2>
-                        <p id="surveyNameText" class="infoLabel">Survey Name: </p>          
-                        <p id="surveyStatusText" class="infoLabel">Survey Status: </p>
-                        <p class="infoLabel font-weight-bold mb-3">Please select the survey to be reset.</p>
-
+                        <p class="norcToolDropdownInfoText">
+                            Please select the survey to be reset below. A survey reset means that the survey data and answers will be <span style="text-decoration:underline;">permanently deleted</span>, and the survey status flag will be reset to 'not started'.
+                        </p>
                         <div style="display:flex">
                             <p class="infoLabel">Update Survey:</p>
                             <div class="btn-group dropright">
@@ -72,6 +70,10 @@ const renderDataCorrectionsSelectionContent = (participant) => {
                                 </div>    
                             </div>
                         </div>
+                        <p class="font-weight-bold mt-5">Current Survey Status</p>
+                        <p id="surveyNameText" class="infoLabel">Survey Name: </p>          
+                        <p id="surveyStatusText" class="infoLabel">Survey Status: </p>
+                        <p id="isSurveyAlreadyResetNote" class="font-weight-bold"></p>
                     </div>
                 </div>
 
@@ -122,6 +124,7 @@ const handleSurveyTypeChange = (participant) => {
     if (!surveyDropdown || !dropdownSurveyOptions || !selectButton) return;
 
     const participantConnectId = participant['Connect_ID'];
+    const { ssnStatusFlag, notStarted } = fieldMapping;
     let query;
 
     for (let option of dropdownSurveyOptions) {
@@ -133,11 +136,19 @@ const handleSurveyTypeChange = (participant) => {
                 try {
                     query = `connectId=${participantConnectId}`
                     showAnimation();
-                    const response =  await findParticipant(query);
+                    const response = await findParticipant(query);
                     hideAnimation();
-                    const latestParticipant = response.data[0];
-                    localStorage.setItem('participant', JSON.stringify(latestParticipant));
-                    updateSurveyStatusTextContent(latestParticipant, selectedSurvey);
+                    const participantData = response.data[0]; // todo change to participantData
+                    localStorage.setItem('participant', JSON.stringify(participantData));
+
+                    if (participantData[ssnStatusFlag] === notStarted) { 
+                        displayAlreadyResetNote();
+                        disableSubmitButton();
+                    } else { 
+                        enableSubmitButton();
+                    }
+                    updateSurveyStatusTextContent(participantData, selectedSurvey);
+                    
                 } catch (error) {
                     console.error(`Failed to fetch participant data for Connect ID ${participantConnectId}: `, error);
                 }
@@ -145,6 +156,7 @@ const handleSurveyTypeChange = (participant) => {
                 selectButton.textContent = e.target.textContent;
                 selectedSurvey = null;
                 updateSurveyStatusTextContent(participant, selectedSurvey);
+                removeAlreadyResetNote();
             }
         });
     }
@@ -158,12 +170,9 @@ const updateSurveyStatusTextContent = (participant, selectedSurvey) => {
     const participantSurveyStatus = {
         "ssn": participant[fieldMapping.ssnStatusFlag],
     };
-
     if (selectedSurvey === 'ssn') {
         surveyNameElement.textContent = 'Survey Name: SSN Survey';
         surveyStatusElement.textContent = `Survey Status: ${statusMapping[participantSurveyStatus.ssn] || ''} `;
-        enableSubmitButton();
-
     } else if (selectedSurvey === null) {
         surveyNameElement.textContent = 'Survey Name: ';
         surveyStatusElement.textContent = 'Survey Status: ';
@@ -180,20 +189,17 @@ const clearSurveySelection = () => {
 
     clearButton.addEventListener('click', () => {
         const surveyNameElement = document.getElementById('surveyNameText');
-        if (!surveyNameElement) return;
         const surveyStatusElement = document.getElementById('surveyStatusText');
-        if (!surveyStatusElement) return
         const selectButton = document.querySelector('.selectButton');
-        if (!selectButton) return;
         const submitButton = document.getElementById('submitButton');
-        if (!submitButton) return;
+        if (!surveyNameElement || !surveyStatusElement || !selectButton || !submitButton) return;
 
         surveyNameElement.textContent = 'Survey Name: ';
         surveyStatusElement.textContent = 'Survey Status: ';
         selectButton.textContent = 'Select';
         selectedSurvey = null;
-        submitButton.disabled = true;
-
+        disableSubmitButton();
+        removeAlreadyResetNote();
     });
 };
 
@@ -215,7 +221,9 @@ const submitSurveyStatusReset = () => {
                     if (response.code === 200 || response.data) {
                         localStorage.setItem('participant', JSON.stringify(response.data));
                         updateSurveyStatusTextContent(response.data, selectedSurvey);
+                        displayAlreadyResetNote();
                         triggerNotificationBanner("Survey has been successfully reset!", "success", 10000);
+                        disableSubmitButton();
                     }
                 }   catch (error) { 
                     console.error(`Failed to reset survey: ${error.message}`);
@@ -225,10 +233,34 @@ const submitSurveyStatusReset = () => {
     }
 };
 
+const displayAlreadyResetNote = () => { 
+    const isSurveyAlreadyResetNote = document.getElementById('isSurveyAlreadyResetNote');
+    if (!isSurveyAlreadyResetNote) return;
+    isSurveyAlreadyResetNote.innerHTML = `<span><i class="fas fa-check-square fa-lg" style="color: #4CAF50; background: white;"></i> The survey is "Not Started". There is no survey data to be reset.</span>`;
+}
+
+const removeAlreadyResetNote = () => { 
+    const isSurveyAlreadyResetNote = document.getElementById('isSurveyAlreadyResetNote');
+    if (!isSurveyAlreadyResetNote) return;
+    isSurveyAlreadyResetNote.innerHTML = ``;
+}
+
 const setupModalContent = (survey) => {
     const modalBody = document.querySelector('.modal-body');
     if (!modalBody) return;
     modalBody.textContent = surveyModalBody[survey];
+}
+
+const disableSubmitButton = () => { 
+    const submitButton = document.getElementById('submitButton');
+    if (!submitButton) return;
+    submitButton.disabled = true;
+}
+
+const enableSubmitButton = () => { 
+    const submitButton = document.getElementById('submitButton');
+    if (!submitButton) return;
+    submitButton.disabled = false;
 }
 
 /**
@@ -266,15 +298,3 @@ const resetParticipantSurvey = async (selectedSurvey) => {
         throw error;
     }
 };
-
-const disableSubmitButton = () => { 
-    const submitButton = document.getElementById('submitButton');
-    if (!submitButton) return;
-    submitButton.disabled = true;
-}
-
-const enableSubmitButton = () => { 
-    const submitButton = document.getElementById('submitButton');
-    if (!submitButton) return;
-    submitButton.disabled = false;
-}
